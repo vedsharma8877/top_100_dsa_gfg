4f85193f1a2439360bc15b875fc420867e0003cd
vedsharma8877 <vedsharma8877@gmail.com>
2024-12-03 17:58:36 +0530

    flipped half diamond

--- Patterns/flipped_half_diamond.cpp
+++ Patterns/flipped_half_diamond.cpp
@@ -1,12 +1,12 @@
-// * 
-// * * 
-// * * * 
-// * * * * 
-// * * * * * 
-// * * * * 
-// * * * 
-// * * 
-// * 
+//     *
+//    **
+//   ***
+//  ****
+// *****
+//  ****
+//   ***
+//    **
+//     *
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,14 +16,20 @@ int main() {
     cout << "Enter a number \n";
     cin >> n;
     for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
         for(int j = 1; j <= i; j++) {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
     for(int i = n-1; i >= 1; i--) {
+        for(int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
         for(int j = 1; j <= i; j++) {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }

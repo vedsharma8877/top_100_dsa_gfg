fc7ea7422602eb5a635a10fe6987345bb2f1cdaa
vedsharma8877 <vedsharma8877@gmail.com>
2024-12-04 18:41:44 +0530

    Numbers Pyramid Pattern without Reassigning

--- Patterns/number_pyramid_2.cpp
+++ Patterns/number_pyramid_2.cpp
@@ -1,19 +1,19 @@
 // 1 
-// 2 2 
-// 3 3 3 
-// 4 4 4 4 
-// 5 5 5 5 5 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
+    int n, num = 1;
     cout << "Enter a number \n";
     cin >> n;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= i; j++) 
-            cout << i <<" ";
+            cout << num++ <<" ";
         cout << endl;
     }
     return 0;

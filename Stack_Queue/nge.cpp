b35cba870644f9c7e38de2797f3d4c5f2eaedb89
Ved Sharma <vedsharma8877@gmail.com>
2025-01-01 23:45:24 +0530

    NGE- Approach 2

--- Stack_Queue/nge.cpp
+++ Stack_Queue/nge.cpp
@@ -9,17 +9,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// void printNGE(int arr[], int n) {
+//     int next;
+//     for(int i = 0; i < n; i++) {
+//         next = -1;
+//         for(int j = i+1; j < n; j++) {
+//             if(arr[i] < arr[j]) {
+//                 next = arr[j];
+//                 break;
+//             }
+//         }
+//         cout << arr[i] << " --> " << next << endl;
+//     }
+// }
+
+// int main() {
+//     int arr[] = {11, 13, 21, 3};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     printNGE(arr, n);
+// }
+
+// Using Stack:
+
 void printNGE(int arr[], int n) {
-    int next;
-    for(int i = 0; i < n; i++) {
-        next = -1;
-        for(int j = i+1; j < n; j++) {
-            if(arr[i] < arr[j]) {
-                next = arr[j];
-                break;
-            }
+    stack<int> s;
+    s.push(arr[0]);
+    for(int i = 1; i < n; i++) {
+        if(s.empty()) {
+            s.push(arr[i]);
+            continue;
         }
-        cout << arr[i] << " --> " << next << endl;
+        while(!s.empty() && s.top() < arr[i]) {
+            cout << s.top() << " --> " << arr[i] << endl;
+            s.pop();
+        }
+        s.push(arr[i]);
+    }
+    while(!s.empty()) {
+        cout << s.top() << " --> " << -1 << endl;
+        s.pop();
     }
 }
 

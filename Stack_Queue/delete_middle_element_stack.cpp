cbc1de39fd2ed0715037b0a092f515b67878bc1a
Ved Sharma <vedsharma8877@gmail.com>
2025-01-04 17:36:16 +0530

    Delete middle element of a stack - Approach 2

--- Stack_Queue/delete_middle_element_stack.cpp
+++ Stack_Queue/delete_middle_element_stack.cpp
@@ -8,38 +8,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    stack<char> st;
-    st.push('1');
-    st.push('2');
-    st.push('3');
-    st.push('9');
-    st.push('8');
-    st.push('6');
-    st.push('7');
-    vector<char> v;
-    while(!st.empty()) {
-        v.push_back(st.top());
+// int main()
+// {
+//     stack<char> st;
+//     st.push('1');
+//     st.push('2');
+//     st.push('3');
+//     st.push('9');
+//     st.push('8');
+//     st.push('6');
+//     st.push('7');
+//     vector<char> v;
+//     while(!st.empty()) {
+//         v.push_back(st.top());
+//         st.pop();
+//     }
+//     int n = v.size();
+//     if(n%2 == 0) {
+//         int target = n/2;
+//         for(int i = 0; i < n; i++) {
+//             if(i == target)
+//                 continue;
+//             st.push(v[i]);
+//         }
+//     }
+//     else {
+//         int target = ceil(n/2);
+//         for(int i = 0; i < n; i++) {
+//             if(i == target)
+//                 continue;
+//             st.push(v[i]);
+//         }
+//     }
+//     while(!st.empty()) {
+//         cout << st.top() << " ";
+//         st.pop();
+//     }
+//     return 0;
+// }
+
+
+// Time Complexity: O(N), For the recursive calls
+// Auxiliary Space: O(N), For the Recursion call Stack
+
+void deleteMidUtil(stack<int>& st, int sizeOfStack, int current) {
+    if(current == sizeOfStack/2) {
         st.pop();
+        return;
     }
-    int n = v.size();
-    if(n%2 == 0) {
-        int target = n/2;
-        for(int i = 0; i < n; i++) {
-            if(i == target)
-                continue;
-            st.push(v[i]);
-        }
-    }
-    else {
-        int target = ceil(n/2);
-        for(int i = 0; i < n; i++) {
-            if(i == target)
-                continue;
-            st.push(v[i]);
-        }
-    }
+    int x = st.top();
+    st.pop();
+    current += 1;
+    deleteMidUtil(st, sizeOfStack, current);
+    st.push(x);
+}
+
+void deleteMid(stack<int>& s, int sizeOfStack) {
+    deleteMidUtil(s, sizeOfStack, 0);
+}
+
+int main() {
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    st.push(7);
+    deleteMid(st, st.size());
     while(!st.empty()) {
         cout << st.top() << " ";
         st.pop();
